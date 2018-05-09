5b8f780073405c738b390de8d6129ce541612a14
stasOR4ik <kov-stas99@mail.ru>
2018-05-09 11:56:53 +0300

    Task_3 - Starting site and Adding ready MusicDB

---
+++ Task_3/MusicSite/MusicSite/MusicDBContext.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace MusicSite
+{
+    public partial class MusicDBContext : DbContext
+    {
+        public MusicDBContext()
+        {
+        }
+
+        public MusicDBContext(DbContextOptions<MusicDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Albums> Albums { get; set; }
+        public virtual DbSet<Singers> Singers { get; set; }
+        public virtual DbSet<Songs> Songs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=MusicDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Albums>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.HasOne(d => d.Singer)
+                    .WithMany(p => p.Albums)
+                    .HasForeignKey(d => d.SingerId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Albums_Singers");
+            });
+
+            modelBuilder.Entity<Singers>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Songs>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.HasOne(d => d.Album)
+                    .WithMany(p => p.Songs)
+                    .HasForeignKey(d => d.AlbumId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Songs_Albums");
+
+                entity.HasOne(d => d.Singer)
+                    .WithMany(p => p.Songs)
+                    .HasForeignKey(d => d.SingerId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Songs_Singers");
+            });
+        }
+    }
+}
---
+++ Task_3/MusicSite/MusicSite/Songs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MusicSite
+{
+    public partial class Songs
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public int SingerId { get; set; }
+        public int AlbumId { get; set; }
+
+        public Albums Album { get; set; }
+        public Singers Singer { get; set; }
+    }
+}
